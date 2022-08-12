0d85fe76aeae8efef4561131d9b863f052fb83de
Fabian Silva <silvafabian74@yahoo.com>
2022-08-12 12:11:12 -0400

    stubb up about page

---
+++ src/pages/PageAbout.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page>
+    <h5>About</h5>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'PageAbout'
+})
+</script>
