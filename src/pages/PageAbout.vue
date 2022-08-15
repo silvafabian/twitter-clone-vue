83e8483550778e58ba7c5ab02d34c689721d092c
Fabian Silva <silvafabian74@yahoo.com>
2022-08-15 09:59:25 -0400

    style about page

--- src/pages/PageAbout.vue
+++ src/pages/PageAbout.vue
@@ -1,13 +1,27 @@
 <template>
-  <q-page>
-    <h5>About</h5>
+  <q-page class="q-pa-lg">
+    <h4 class="q-mt-none q-mb-md text-weight-bold">About Twitter Vue</h4>
+    <div class="text-body1">
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+        praesentium maiores nam corporis vel facilis, nostrum temporibus at ab
+        laborum esse velit iste eius voluptatibus possimus! Nobis blanditiis quod
+        similique!
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+        praesentium maiores nam corporis vel facilis, nostrum temporibus at ab
+        laborum esse velit iste eius voluptatibus possimus! Nobis blanditiis quod
+        similique!
+      </p>
+    </div>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'PageAbout'
-})
+  name: "PageAbout",
+});
 </script>
