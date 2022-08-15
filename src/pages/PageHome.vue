adad0c54b0adc6069004558f86ffaa5ee0da1fb3
Fabian Silva <silvafabian74@yahoo.com>
2022-08-15 16:07:42 -0400

    add final styling touches

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,84 +1,98 @@
 <template>
-  <q-page>
-    <div class="q-py-lg q-px-md row items-end q-col-gutter-md">
-      <div class="col">
-        <q-input
-          v-model="newTwitContent"
-          class="new-tweet"
-          bottom-slots
-          counter
-          autogrow
-          placeholder="What's Happening?"
-          maxlength="280"
+  <q-page class="relative-position">
+    <q-scroll-area class="absolute fullscreen">
+      <div class="q-py-lg q-px-md row items-end q-col-gutter-md">
+        <div class="col">
+          <q-input
+            v-model="newTwitContent"
+            class="new-tweet"
+            bottom-slots
+            counter
+            autogrow
+            placeholder="What's Happening?"
+            maxlength="280"
+          >
+            <template v-slot:before>
+              <q-avatar size="xl">
+                <img src="https://cdn.quasar.dev/img/avatar5.jpg" />
+              </q-avatar>
+            </template>
+          </q-input>
+        </div>
+
+        <div class="col col-shrink">
+          <q-btn
+            @click="addNewTweet"
+            :disable="!newTwitContent"
+            class="q-mb-lg"
+            unelevated
+            rounded
+            no-caps
+            color="primary"
+            label="Tweet Vue"
+          />
+        </div>
+      </div>
+
+      <q-separator size="10px" color="grey-2" class="divider" />
+
+      <q-list separator>
+        <transition-group
+          appear
+          enter-active-class="animated fadeIn slow"
+          leave-active-class="animated fadeOut"
         >
-          <template v-slot:before>
-            <q-avatar size="xl">
-              <img src="https://cdn.quasar.dev/img/avatar5.jpg" />
-            </q-avatar>
-          </template>
-        </q-input>
-      </div>
+          <q-item v-for="tweet in tweets" :key="tweet.date" class="q-py-md">
+            <q-item-section avatar top>
+              <q-avatar>
+                <img src="https://cdn.quasar.dev/img/avatar5.jpg" />
+              </q-avatar>
+            </q-item-section>
 
-      <div class="col col-shrink">
-        <q-btn
-          @click="addNewTweet"
-          :disable="!newTwitContent"
-          class="q-mb-lg"
-          unelevated
-          rounded
-          no-caps
-          color="primary"
-          label="Tweet Vue"
-        />
-      </div>
-    </div>
+            <q-item-section>
+              <q-item-label class="text-subtitle1">
+                <strong> Fabian Silva </strong>
+                <span class="text-grey-7">
+                  @fabiansilva
+                  <br class="lt-md" />
+                  &bull; {{ relativeDate(tweet.date) }}
+                </span>
+              </q-item-label>
+              <q-item-label class="tweet-content text-body1">
+                {{ tweet.content }}
+              </q-item-label>
+              <div class="tweet-icons row justify-between q-mt-sm">
+                <q-btn
+                  flat
+                  round
+                  color="grey"
+                  icon="far fa-comment"
+                  size="sm"
+                />
+                <q-btn
+                  flat
+                  round
+                  color="grey"
+                  icon="fas fa-retweet"
+                  size="sm"
+                />
+                <q-btn flat round color="grey" icon="far fa-heart" size="sm" />
+                <q-btn
+                  @click="deleteTweet(tweet)"
+                  flat
+                  round
+                  color="grey"
+                  icon="fas fa-trash"
+                  size="sm"
+                />
+              </div>
+            </q-item-section>
+          </q-item>
+        </transition-group>
 
-    <q-separator size="10px" color="grey-2" class="divider" />
-
-    <q-list separator>
-      <transition-group
-        appear
-        enter-active-class="animated fadeIn slow"
-        leave-active-class="animated fadeOut"
-      >
-        <q-item v-for="tweet in tweets" :key="tweet.date" class="q-py-md">
-          <q-item-section avatar top>
-            <q-avatar>
-              <img src="https://cdn.quasar.dev/img/avatar5.jpg" />
-            </q-avatar>
-          </q-item-section>
-
-          <q-item-section>
-            <q-item-label class="text-subtitle1">
-              <strong> Fabian Silva </strong>
-              <span class="text-grey-7"> @fabiansilva </span>
-            </q-item-label>
-            <q-item-label class="tweet-content text-body1">
-              {{ tweet.content }}
-            </q-item-label>
-            <div class="tweet-icons row justify-between q-mt-sm">
-              <q-btn flat round color="grey" icon="far fa-comment" size="sm" />
-              <q-btn flat round color="grey" icon="fas fa-retweet" size="sm" />
-              <q-btn flat round color="grey" icon="far fa-heart" size="sm" />
-              <q-btn
-                @click="deleteTweet(tweet)"
-                flat
-                round
-                color="grey"
-                icon="fas fa-trash"
-                size="sm"
-              />
-            </div>
-          </q-item-section>
-
-          <q-item-section side top>
-            {{ relativeDate(tweet.date) }}
-          </q-item-section>
-        </q-item>
-      </transition-group>
-
-      <q-separator inset="item" />
-    </q-list>
+        <q-separator inset="item" />
+      </q-list>
+    </q-scroll-area>
   </q-page>
 </template>
 
